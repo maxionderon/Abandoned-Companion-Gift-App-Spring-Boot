11d00d17ec273a447666b3cd99724822eaf44c3c
maxionderon <ch-mp@web.de>
2019-09-21 15:34:55 +0200

    first commit

---
+++ src/test/java/dev/maxionderon/companiongiftapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maxionderon.companiongiftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
